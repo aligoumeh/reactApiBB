3be109334d2df4048c8c22abd784340fc1ff3094
alig <475441@student.saxion.nl>
2021-11-20 20:45:39 +0100

    axios with api added

--- reactapi/src/App.js
+++ reactapi/src/App.js
@@ -1,8 +1,22 @@
-import React from "react"
+import React, {useState, useEffect} from "react"
 import Header from "./components/Header"
+import axios from "axios"
 import './App.css';
 
 const App = () => {
+  const [items, setItems] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      const results = await axios(`https://www.breakingbadapi.com/api/characters`)
+
+      console.log(results.data)
+    }
+
+    fetchItems()
+  }, [])
+
   return (
     <div className="container">
       <Header />
