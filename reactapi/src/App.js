49fcd7368801ebf1b0e326d2df604b4f6797d05c
alig <475441@student.saxion.nl>
2021-11-20 21:03:21 +0100

    final touch added

--- reactapi/src/App.js
+++ reactapi/src/App.js
@@ -1,31 +1,38 @@
-import React, {useState, useEffect} from "react"
-import Header from "./components/Header"
-import Characters from "./components/Characters"
-import axios from "axios"
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import Header from './components/Header'
+import CharacterGrid from './components/Characters'
+import Search from './components/Search'
+import './App.css'
 
 const App = () => {
   const [items, setItems] = useState([])
-  const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(true)
+  const [query, setQuery] = useState('')
 
   useEffect(() => {
     const fetchItems = async () => {
-      const results = await axios(`https://www.breakingbadapi.com/api/characters`)
+      setIsLoading(true)
+      const result = await axios(
+        `https://www.breakingbadapi.com/api/characters?name=${query}`
+      )
 
-      console.log(results.data)
-      setItems(results.data)
+      console.log(result.data)
+
+      setItems(result.data)
       setIsLoading(false)
     }
 
     fetchItems()
-  }, [])
+  }, [query])
 
   return (
-    <div className="container">
+    <div className='container'>
       <Header />
-      <Characters isLoading={isLoading} items={items}/>
+      <Search getQuery={(q) => setQuery(q)} />
+      <CharacterGrid isLoading={isLoading} items={items} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
